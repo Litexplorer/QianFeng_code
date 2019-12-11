41ce308e4d658c16500cd9eb0e29094b35725711
Litexplorer <1185964443@qq.com>
2019-12-11 20:14:38 +0800

    不知道干了什么

---
+++ 01 前锋教育——李卫民/01 单体应用/my-shop/src/main/java/com/chen/my/shop/bean/User.java
@@ -0,0 +1,19 @@
+package com.chen.my.shop.bean;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @Author: ChromeChen
+ * @Description:
+ * @Date: Created in 20:49 2019-10-23
+ * @Modified By:
+ */
+@Data
+@Accessors(chain = true)
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ 01 前锋教育——李卫民/02 Java全栈/03 Java微服务框架/05 SpringCloud/hello-spring-cloud/hello-spring-cloud-zuul/src/main/java/com/chen/hello/spring/cloud/zuul/ZuulApplication.java
@@ -0,0 +1,22 @@
+package com.chen.hello.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author: ChromeChen
+ * @Description:
+ * @Date: Created in 17:41 2019/10/21 0021
+ * @Modified By:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
