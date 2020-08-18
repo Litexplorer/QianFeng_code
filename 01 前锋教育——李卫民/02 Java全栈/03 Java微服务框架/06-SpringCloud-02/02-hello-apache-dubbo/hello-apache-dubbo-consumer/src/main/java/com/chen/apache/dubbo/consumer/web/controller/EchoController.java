5bcb401a4d1b90d14964b6c4367d71871dd7f69b
Litexplorer <20152100190@m.scnu.edu.cn>
2020-08-19 01:04:17 +0800

    :new: 添加了『nacos 分布式配置中心』的代码

--- 01 前锋教育——李卫民/02 Java全栈/03 Java微服务框架/06-SpringCloud-02/02-hello-apache-dubbo/hello-apache-dubbo-consumer/src/main/java/com/chen/apache/dubbo/consumer/web/controller/EchoController.java
+++ 01 前锋教育——李卫民/02 Java全栈/03 Java微服务框架/06-SpringCloud-02/02-hello-apache-dubbo/hello-apache-dubbo-consumer/src/main/java/com/chen/apache/dubbo/consumer/web/controller/EchoController.java
@@ -2,6 +2,8 @@ package com.chen.apache.dubbo.consumer.web.controller;
 
 import com.chen.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @Modified By:
  */
 @RestController
+@RefreshScope
 @RequestMapping("/echo")
 public class EchoController {
+
     @Reference(version = "1.0.0")
     private EchoService echoService;
+
+    @Value("${user.name}")
+    private String username;
 
     @GetMapping(value = "/{string}")
     public String echo(@PathVariable String string) {
         return echoService.echo(string);
     }
+
+    
+    @GetMapping(value = "/refresh")
+    public String refresh() {
+        return echoService.echo("consumer message is : " + username);
+    }
+
 }
